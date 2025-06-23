04868f1a2dcf63725f8f68e054160d353fbbbafa
Ayush Kumar <ayushsuperstar48@gmail.com>
2025-06-23 11:09:54 +0530

    updated dev environments

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -9,6 +9,7 @@ import {
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
 } from "$env/static/private";
+import { dev } from "$app/environment";
 
 if (!DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
@@ -19,10 +20,14 @@ export const db = drizzle(dbClient, { schema });
 export const googleAuth = new Google(
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
-  "http://localhost:5173/login/google/callback",
+  dev
+    ? "http://localhost:5173/login/google/callback"
+    : "https://zeronoticed.vercel.app/login/google/callback",
 );
 export const githubAuth = new GitHub(
   GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET,
-  "http://localhost:5173/login/github/callback",
+  dev
+    ? "http://localhost:5173/login/github/callback"
+    : "https://zeronoticed.vercel.app/login/github/callback",
 );
